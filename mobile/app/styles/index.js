58cf8aa66b772eb2b6049976a2621c04b616b40f
Luiz Guilherme Picorelli <picorelli.luiz@gmail.com>
2018-07-18 10:35:22 -0300

    Add styles

---
+++ mobile/app/styles/index.js
@@ -0,0 +1,36 @@
+export const colors = {
+  primary: 'rgba(213, 55, 43, 1)',
+  secondary: 'rgba(255, 255, 255, 1)',
+  ternary: 'rgba(224, 31, 35, 1)',
+  quarternary: 'rgba(0, 212, 115, 1)',
+}
+
+export const fontSize = {
+  sm: 16,
+  md: 28,
+  lg: 38,
+  xl: 58,
+}
+
+export const padding = {
+  xs: 5,
+  sm: 10,
+  md: 20,
+  lg: 30,
+  xl: 40,
+}
+
+export const borderRadius = {
+  xs: 0,
+  sm: 8,
+  md: 10,
+  lg: 12,
+  xl: 14,
+}
+
+export const alignVh = {
+  flex: 1,
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+}
